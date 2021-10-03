8a348156ee54c8a441b04982f0f153da4ee26374
kshirvanyan <kshirvanyan@questrade.com>
2021-10-03 19:17:33 +0000

    add auth0 integration

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
+import LoginButton from './components/loginbutton';
+import LogoutButton from './components/logoutbutton';
+import Profile from './components/profile';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <LoginButton/>
+      </div>
+      <div>
+        <LogoutButton/>
+      </div>
+      <div>
+        <Profile></Profile>
+      </div>
     </div>
+
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain="dev-o6ibqddz.us.auth0.com"
+    clientId="7zt82aQSGQBcp3JhKXqmJr6NgRQyuQTh"
+    redirectUri="http://localhost:3000"
+  >
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Auth0Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
